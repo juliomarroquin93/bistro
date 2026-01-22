c07b1596267de95c35cfb10718023838041f7ef4
Julio Marroquin <juliomarroquin1993@hotmail.com>
2026-01-21 19:12:41 -0600

    subpedidos

--- assets/js/modulos/pedidos.js
+++ assets/js/modulos/pedidos.js
@@ -125,6 +125,16 @@ document.addEventListener('DOMContentLoaded', function () {
 		 mostrarProducto();
 
 		  })
+
+		  document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.btn-subpedido').forEach(function(btn) {
+        btn.addEventListener('click', function(e) {
+            e.preventDefault();
+            const id = btn.getAttribute('data-id');
+            window.location.href = base_url + 'pedidos/subpedido/' + id;
+        });
+    });
+});
 		  
 		   docuemi.addEventListener('click', function () {
 		if(docuemi.value == "FACTURA"){	   
