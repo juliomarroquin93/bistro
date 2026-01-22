fd142eeff1882534c3f7127ea07a9c535c625dd0
Julio Marroquin <juliomarroquin1993@hotmail.com>
2026-01-22 01:28:19 -0600

    Fix template

--- views/templates/header.php
+++ views/templates/header.php
@@ -190,69 +190,149 @@
                 <li>
                     <a href="<?php echo BASE_URL . 'librosIva'; ?>">
                         <div class="parent-icon"><i class="fa-solid fa-file-lines"></i>
-                        </div>
-                        <div class="menu-title">Libros de IVA</div>
-                    </a>
-                </li>
-
-                <?php }  if (verificar('inventario y kardex')) { ?>
-                <li>
-                    <a href="<?php echo BASE_URL . 'inventarios'; ?>">
-                        <div class="parent-icon"><i class="fa-solid fa-list-check"></i>
-                        </div>
-                        <div class="menu-title">Inventario & Kardex</div>
-                    </a>
-                </li>
-                <?php }  if (verificar('Cortez')) { ?> 
-				<li>
-                    <a href="<?php echo BASE_URL . 'cortez'; ?>">
-                        <div class="parent-icon"><i class="fa-solid fa-circle-dollar-to-slot"></i>
-                        </div>
-                        <div class="menu-title">Corte Z</div>
-                    </a>
-                </li>
-                 <?php } if (verificar('catalogoCuentas')) { ?> 
-				<li>
-                    <a href="<?php echo BASE_URL . 'catalogoCuentas'; ?>">
-                        <div class="parent-icon"><i class="fa-solid fa-chalkboard-user"></i>
-                        </div>
-                        <div class="menu-title">Catalogo de Cuentas</div>
-                    </a>
-                </li>
-                <?php } ?>		  
-            </ul>
-            
-            <!--end navigation-->
-        </div>
-        <!--end sidebar wrapper -->
-        <!--start header -->
-        <header>
-            <div class="topbar d-flex align-items-center">
-                <nav class="navbar navbar-expand">
-                    <div class="mobile-toggle-menu"><i class='bx bx-menu'></i>
-                    </div>
-                    <div class="search-bar flex-grow-1">
-                        <div class="position-relative">
-                            <h6><?php echo TITLE; ?></h6>
-                        </div>
-                    </div>
-                    <div class="user-box dropdown">
-                        <a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <?php if ($_SESSION['perfil_usuario'] == null) {
-                                $perfil = BASE_URL . 'assets/images/logo.png';
-                            } else {
-                                $perfil = BASE_URL . $_SESSION['perfil_usuario'];
-                            } ?>
-                            <img src="<?php echo $perfil; ?>" class="user-img" alt="user avatar">
-                            <!-- Menú Bootstrap Moderno -->
-                            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-                                <div class="container-fluid">
-                                    <a class="navbar-brand" href="#">Bistro</a>
-                                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                        <span class="navbar-toggler-icon"></span>
-                                    </button>
-                                    <div class="collapse navbar-collapse" id="navbarNav">
-                                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <!-- Aquí continúa el contenido de la página principal -->
+                                            <li> <a href="<?php echo BASE_URL . 'salidas'; ?>"><i class="bx bx-right-arrow-alt"></i>Movimientos</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <?php } if (verificar('ingresos') && ! verificar('salidas') ) { ?>
+                                    </ul>
+                                    </li>
+                                    <?php } if ((verificar('pedidos')) || (verificar('FacturarPedidos')) || (verificar('despacho')) ) { ?>
+                                    <li>
+                                        <a href="javascript:;" class="has-arrow">
+                                            <div class="parent-icon"><i class="fa-regular fa-id-badge"></i>
+                                            </div>
+                                            <div class="menu-title">Pedidos</div>
+                                        </a>
+                                        <ul>
+                                            <?php } if (verificar('pedidos')) { ?>
+                                            <li> <a href="<?php echo BASE_URL . 'pedidos'; ?>"><i class="bx bx-right-arrow-alt"></i>Nuevo Pedido</a>
+                                            </li>
+                           
+                                            <?php } if (verificar('FacturarPedidos')) { ?>
+                                            <li> <a href="<?php echo BASE_URL . 'pedidos/facturacion'; ?>"><i class="bx bx-right-arrow-alt"></i>Facturacion</a>
+                                            </li>
+                                            <?php } if (verificar('despacho')) { ?>
+                                            <li> <a href="<?php echo BASE_URL . 'pedidos/despacho'; ?>"><i class="bx bx-right-arrow-alt"></i>Despacho</a>
+                                            </li>
+                                    <?php } if (verificar('pedidos') || verificar('FacturarPedidos') || verificar('despacho') ) { ?>
+                                    </ul>
+                                    </li>
+                                <?php } if (verificar('compras')) { ?>
+                
+                                <li>
+                                    <a href="<?php echo BASE_URL . 'ventas2'; ?>">
+                                        <div class="parent-icon"><i class="fa-solid fa-credit-card"></i>
+                                        </div>
+                                        <div class="menu-title">Compras</div>
+                                    </a>
+                                </li>
+                                <?php }  if (verificar('ventas')) { ?>
+                                <li>
+                                    <a href="<?php echo BASE_URL . 'ventas'; ?>">
+                                        <div class="parent-icon"><i class="fa-solid fa-cash-register"></i>
+                                        </div>
+                                        <div class="menu-title">Ventas</div>
+                                    </a>
+                                </li>
+                                 <?php }  if (verificar('requisiciones')) { ?>
+                                <li>
+                                    <a href="<?php echo BASE_URL . 'requisiciones'; ?>">
+                                        <div class="parent-icon"><i class="fa-solid fa-cart-shopping"></i>
+                                        </div>
+                                        <div class="menu-title">Requisiciones</div>
+                                    </a>
+                                </li>
+                                <?php }  if (verificar('OrdenesCompras')) { ?>
+                                <li>
+                                    <a href="<?php echo BASE_URL . 'ordenesCompra/listado'; ?>">
+                                        <div class="parent-icon"><i class="fa-solid fa-file-invoice-dollar"></i>
+                                        </div>
+                                        <div class="menu-title">Órdenes de Compra</div>
+                                    </a>
+                                </li>
+                                <?php }if (verificar('Nota de credito')) { ?>
+                                <li>
+                                    <a href="<?php echo BASE_URL . 'notaCredito'; ?>">
+                                        <div class="parent-icon"><i class="fa-solid fa-bars-staggered"></i>
+                                        </div>
+                                        <div class="menu-title">Nota de crédito</div>
+                                    </a>
+                                </li>
+                                <?php }  if (verificar('cuentas por cobrar')) { ?>
+                                <li>
+                                    <a href="<?php echo BASE_URL . 'creditos'; ?>">
+                                        <div class="parent-icon"><i class="fa-solid fa-hand-holding-heart"></i>
+                                        </div>
+                                        <div class="menu-title">Cuentas por cobrar</div>
+                                    </a>
+                                </li>
+                                <?php }  if (verificar('cuentas por pagar')) { ?>
+                                <li>
+                                    <a href="<?php echo BASE_URL . 'creditos2'; ?>">
+                                        <div class="parent-icon"><i class="fa-solid fa-money-bill-wave"></i>
+                                        </div>
+                                        <div class="menu-title">Cuentas por pagar</div>
+                                    </a>
+                                </li>
+                                <?php }  if (verificar('formulario medico')) { ?>
+                                <li>
+                                    <a href="<?php echo BASE_URL . 'cotizaciones2'; ?>">
+                                        <div class="parent-icon"><i class="fa-solid fa-glasses"></i>
+                                        </div>
+                                        <div class="menu-title">Formulario Medico</div>
+                                    </a>
+                                </li>
+                                <?php }  if (verificar('cotizaciones')) { ?>
+                                <li>
+                                    <a href="<?php echo BASE_URL . 'cotizaciones'; ?>">
+                                        <div class="parent-icon"><i class="fa-solid fa-book-open-reader"></i>
+                                        </div>
+                                        <div class="menu-title">Cotizaciones</div>
+                                    </a>
+                                </li>
+                                <?php }  if (verificar('apartados')) { ?>
+                                <li>
+                                    <a href="<?php echo BASE_URL . 'apartados'; ?>">
+                                        <div class="parent-icon"><i class="fa-solid fa-people-arrows"></i>
+                                        </div>
+                                        <div class="menu-title">Apartados</div>
+                                    </a>
+                                </li>
+                                <?php }  if (verificar('librosIva')) { ?>
+                                <li>
+                                    <a href="<?php echo BASE_URL . 'librosIva'; ?>">
+                                        <div class="parent-icon"><i class="fa-solid fa-file-lines"></i>
+                                        </div>
+                                        <div class="menu-title">Libros de IVA</div>
+                                    </a>
+                                </li>
+                                <?php }  if (verificar('inventario y kardex')) { ?>
+                                <li>
+                                    <a href="<?php echo BASE_URL . 'inventarios'; ?>">
+                                        <div class="parent-icon"><i class="fa-solid fa-list-check"></i>
+                                        </div>
+                                        <div class="menu-title">Inventario & Kardex</div>
+                                    </a>
+                                </li>
+                                <?php }  if (verificar('Cortez')) { ?> 
+                				<li>
+                                    <a href="<?php echo BASE_URL . 'cortez'; ?>">
+                                        <div class="parent-icon"><i class="fa-solid fa-circle-dollar-to-slot"></i>
+                                        </div>
+                                        <div class="menu-title">Corte Z</div>
+                                    </a>
+                                </li>
+                                 <?php } if (verificar('catalogoCuentas')) { ?> 
+                				<li>
+                                    <a href="<?php echo BASE_URL . 'catalogoCuentas'; ?>">
+                                        <div class="parent-icon"><i class="fa-solid fa-chalkboard-user"></i>
+                                        </div>
+                                        <div class="menu-title">Catalogo de Cuentas</div>
+                                    </a>
+                                </li>
+                                <?php } ?>		  
                                             <li class="nav-item">
                                                 <a class="nav-link active" aria-current="page" href="<?php echo BASE_URL; ?>admin">Inicio</a>
                                             </li>
